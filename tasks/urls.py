3c1f17c9ecb748f3316ce1f9970421b5e6bbe922
David <fankmen@gmail.com>
2024-05-30 19:04:27 +0200

    corrected urls and views

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
+from .views import TaskViewSet, index
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
